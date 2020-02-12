eacbb89582307665cbfcff5247a463604e8b694c
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-02-12 10:26:59 -0800

    Added javascript & navbar elements

---
+++ script.js
@@ -0,0 +1,7 @@
+function showHideNav() {
+    if (document.getElementById('nav-dropdown').style.display === 'none') {
+        document.getElementById('nav-dropdown').style.display = 'initial';
+    } else {
+        document.getElementById('nav-dropdown').style.display = 'none';
+    }
+}
