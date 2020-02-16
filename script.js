2f9611d3dba83a77fb75bd68e0aea93039d03f69
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-02-15 22:06:59 -0800

    Developement message

--- script.js
+++
@@ -1,7 +0,0 @@
-function showHideNav() {
-    if (document.getElementById('nav-dropdown').style.display === 'none') {
-        document.getElementById('nav-dropdown').style.display = 'initial';
-    } else {
-        document.getElementById('nav-dropdown').style.display = 'none';
-    }
-}
