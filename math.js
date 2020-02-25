1949e1d79e2c84933c834a219c30751a62bd4c86
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-02-24 21:36:44 -0800

    Created calculator pages and distance formula function

--- math.js
+++ math.js
@@ -11,3 +11,19 @@ function pythag(a,b,c) {
         return Math.sqrt(c);
     }
 }
+
+//distance formula
+function distance() {
+    var x1, x2, y1, y2;
+    x1 = document.getElementById('x1').value;
+    x2 = document.getElementById('x2').value;
+    y1 = document.getElementById('y1').value;
+    y2 = document.getElementById('y2').value;
+    document.getElementById('distance-solution').innerHTML = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+}
+function clearDistance() {
+    document.getElementById('x1').value = '';
+    document.getElementById('x2').value = '';
+    document.getElementById('y1').value = '';
+    document.getElementById('y2').value = '';
+}
