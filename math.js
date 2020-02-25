811fc327f963b0ef023f7b690c0f1461eeeddc1c
Sutantu Balamurugan <sutan04@gmail.com>
2020-02-25 15:08:34 -0800

    Automatically should resolve

--- math.js
+++ math.js
@@ -1,4 +1,4 @@
-//Multistep Functions
+//Algebra Multistep Functions
 function pythag(a,b,c) {
     if(a === '' || a === null) {
         a = Math.pow(c,2) - Math.pow(b,2);
@@ -10,4 +10,77 @@ function pythag(a,b,c) {
         c = Math.pow(a,2) + Math.pow(b,2);
         return Math.sqrt(c);
     }
+}
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+//Pre-Calculus Multistep Functions
+function parametric(x1,y1,x2,y2,t) {
+    b = (x2-x1)/t;
+    d = (y2-y1)/t;
+    ftb = 't';
+    ftd = 't';
+    if(x1 === 0) {
+        x1 = '';
+    }
+    if(y1 === 0) {
+        y1 = '';
+    }
+    if(b>0) {
+        b = '+' + b;
+    } else if(b === 0) {
+        b = '';
+        ftb = '';
+    } else if(b === 1) {
+        b = '';
+        ftb = '+t';
+    } else if(b === -1) {
+        b = '';
+        ftb = '-t';
+    }
+    if(d>0) {
+        d = '+' + d;
+    } else if(d === 0) {
+        d = '';
+        ftd = '';
+    } else if(d === 1) {
+        d = '';
+        ftd = '+t';
+    } else if(d === -1) {
+        d = '';
+        ftd = '-t';
+    }
+    return '(' + x1 + b + ftb + ', ' + y1 + d + ftd + ')';
+=======
+=======
+>>>>>>> bcc3b41b81772487a28960823c93a61232f696a9
+//distance formula
+function distance() {
+    var x1, x2, y1, y2;
+    x1 = document.getElementById('x1').value;
+    x2 = document.getElementById('x2').value;
+    y1 = document.getElementById('y1').value;
+    y2 = document.getElementById('y2').value;
+<<<<<<< HEAD
+    document.getElementById('distance-solution').innerHTML = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+=======
+    if(x1 === '' || x2 === '' || y1 === '' || y2 === '') {
+        document.getElementById('distance-solution').style.color = '#ff0000';
+        document.getElementById('distance-solution').innerHTML = 'Missing Values';
+    } else {
+        document.getElementById('distance-solution').style.color = '#000000';
+        document.getElementById('distance-solution').innerHTML = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+    }
+>>>>>>> bcc3b41b81772487a28960823c93a61232f696a9
+}
+function clearDistance() {
+    document.getElementById('x1').value = '';
+    document.getElementById('x2').value = '';
+    document.getElementById('y1').value = '';
+    document.getElementById('y2').value = '';
+<<<<<<< HEAD
+>>>>>>> 1949e1d79e2c84933c834a219c30751a62bd4c86
+=======
+    document.getElementById('distance-solution').innerHTML = '';
+>>>>>>> bcc3b41b81772487a28960823c93a61232f696a9
 }
