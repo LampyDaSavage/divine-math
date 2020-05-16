0252f5fc181053edd587800c9fd89ec10683d0be
RedNova64 <57379272+RedNova64@users.noreply.github.com>
2020-05-15 21:02:44 -0700

    Updated calculators, seperated about/contact pages
    Staff members & descriptions moved to about us page. Modified pythag calc for use on in the html web page. Tweeked class names in css doc.

--- math.js
+++ math.js
@@ -1,16 +1,75 @@
+//decimal formating
+function roundDecimal0(x) {
+    x = Math.round(x);
+}
+function roundDecimal1(x) {
+    x = Math.round(x*10)/10;
+}
+function roundDecimal2(x) {
+    x = Math.round(x*100)/100;
+}
+function roundDecimal3(x) {
+    x = Math.round(x*1000)/1000;
+}
+function roundDecimal4(x) {
+    x = Math.round(x*10000)/10000;
+}
+
 //Algebra Multistep Functions
-function pythag(a,b,c) {
-    if(a === '' || a === null) {
-        a = Math.pow(c,2) - Math.pow(b,2);
-        return Math.sqrt(a);
-    } else if(b === '' || b === null) {
-        b = Math.pow(c,2) - Math.pow(a,2);
-        return Math.sqrt(b);
+function pythag() {
+    var a, b, c;
+    a = document.getElementById('a').value;
+    b = document.getElementById('b').value;
+    c = document.getElementById('c').value;
+    if(a === '' && b === '' || b === '' && c === '' || a === '' && c === '' || a === '' && b === '' && c === '') {
+        document.getElementById('pythag-solution').style.color = '#ff0000';
+        document.getElementById('pythag-solution').innerHTML = 'Not enough information';
+    } else if(a !== '' && b !== '' && c !== '') {
+        document.getElementById('pythag-solution').style.color = '#ff0000';
+        document.getElementById('pythag-solution').innerHTML = 'All sides are already known';
+    } else if(c !== '' && (a > c || b > c)) {
+        document.getElementById('pythag-solution').style.color = '#ff0000';
+        document.getElementById('pythag-solution').innerHTML = 'Hypotenuse (c) cannot be less than the other sides (a & b)';
     } else {
-        c = Math.pow(a,2) + Math.pow(b,2);
-        return Math.sqrt(c);
+            document.getElementById('pythag-solution').style.color = '#000000';
+        if(a === '' || a === null) {
+            document.getElementById('pythag-solution').innerHTML = Math.sqrt(Math.pow(c,2) - Math.pow(b,2));
+        } else if(b === '' || b === null) {
+            document.getElementById('pythag-solution').innerHTML = Math.sqrt(Math.pow(c,2) - Math.pow(a,2));
+        } else {
+            document.getElementById('pythag-solution').innerHTML = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+        }
     }
 }
+function resetPythag() {
+    document.getElementById('a').value = '';
+    document.getElementById('b').value = '';
+    document.getElementById('c').value = '';
+    document.getElementById('pythag-solution').innerHTML = '';
+}
+
+function distance() {
+    var x1, x2, y1, y2;
+    x1 = document.getElementById('x1').value;
+    x2 = document.getElementById('x2').value;
+    y1 = document.getElementById('y1').value;
+    y2 = document.getElementById('y2').value;
+    if(x1 === '' || x2 === '' || y1 === '' || y2 === '') {
+        document.getElementById('distance-solution').style.color = '#ff0000';
+        document.getElementById('distance-solution').innerHTML = 'Missing Values';
+    } else {
+        document.getElementById('distance-solution').style.color = '#000000';
+        document.getElementById('distance-solution').innerHTML = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+    }
+}
+function resetDistance() {
+    document.getElementById('x1').value = '';
+    document.getElementById('x2').value = '';
+    document.getElementById('y1').value = '';
+    document.getElementById('y2').value = '';
+    document.getElementById('distance-solution').innerHTML = '';
+}
+
 //Pre-Calculus Multistep Functions
 function parametric(x1,y1,x2,y2,t) {
     b = (x2-x1)/t;
@@ -48,28 +107,4 @@ function parametric(x1,y1,x2,y2,t) {
         ftd = '-t';
     }
     return '(' + x1 + b + ftb + ', ' + y1 + d + ftd + ')';
-}
-//distance formula
-function distance() {
-    var x1, x2, y1, y2;
-    x1 = document.getElementById('x1').value;
-    x2 = document.getElementById('x2').value;
-    y1 = document.getElementById('y1').value;
-    y2 = document.getElementById('y2').value;
-    document.getElementById('distance-solution').innerHTML = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-    if(x1 === '' || x2 === '' || y1 === '' || y2 === '') {
-        document.getElementById('distance-solution').style.color = '#ff0000';
-        document.getElementById('distance-solution').innerHTML = 'Missing Values';
-    } else {
-        document.getElementById('distance-solution').style.color = '#000000';
-        document.getElementById('distance-solution').innerHTML = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-    }
-}
-function clearDistance() {
-    document.getElementById('x1').value = '';
-    document.getElementById('x2').value = '';
-    document.getElementById('y1').value = '';
-    document.getElementById('y2').value = '';
-    document.getElementById('distance-solution').innerHTML = '';
-    document.getElementById('distance-solution').innerHTML = '';
 }
